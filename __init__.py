a98ed38ae63e2f51cbcfa1df4bd169bd98f65b41
marlinarnz <42812798+marlinarnz@users.noreply.github.com>
2018-12-20 10:55:17 +0100

    Initial commit
    Added classes for constants

---
+++ __init__.py
@@ -0,0 +1,10 @@
+''' The constants class contains all necessary constant numbers, categorizations and suchlike.
+The error class contains '''
+
+class Constants:
+
+    CATS = ["Werkstatt Rent", "Trübsee", "Saisonnier m", "Saisonnier w"]
+
+class Errors:
+
+    WRONG_CAT = "Falsche Kategorie"
