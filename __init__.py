761cd17ed5b59f6f2821fd727b20b29929f30e51
marlinarnz <42812798+marlinarnz@users.noreply.github.com>
2018-12-21 14:39:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,21 @@ class Constants:
     # Keys for the params.json file
     JOBS = "jobs"
     UT = "utility"
+    UT_OFF_ROW = "off_in_row"
+    UT_OFF_PRIO1 = "off_prio1"
+    UT_OFF_PRIO2 = "off_prio2"
+    UT_OFF_PRIO3 = "off_prio3"
+    
+    # Keys for the calendar dict
+    WEEKDAY = "weekday"
+    
+    # Keys for the staff dict
+    WORKDAYS = "working days per week"
+    PRIO_JOB = "prioritized job"
+    WISHES = "day off wiches list"
+    PRIO1 = "day off priority 1"
+    PRIO2 = "day off priority 2"
+    PRIO3 = "day off priority 3"
 
 class Errors:
     ''' This class contains error messages '''
