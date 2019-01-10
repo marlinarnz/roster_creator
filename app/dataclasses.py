3cb10ef6ed4a3a6746f82b8d4b21e5d8959001b2
marlinarnz <42812798+marlinarnz@users.noreply.github.com>
2019-01-10 13:16:19 +0100

    Initial commit

---
+++ app/dataclasses.py
@@ -0,0 +1,11 @@
+class Staff:
+    ''' Object representing a staff member with roles, wishes and other specs'''
+
+    def __init__(self, name, workdays, jobs, wishes, prio_job=None):
+        ''' Constructor
+        :param name: String giving the name of the person
+        :param workdays: int defining the working days per week (7 days)
+        :param jobs: list of jobs that can be attended
+        :param wishes: dict with date objects as keys and a priorities as values
+        :param prio_job: job where this person is prioritized'''
+        pass
