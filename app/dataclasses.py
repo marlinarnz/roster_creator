a5ca814104c66ae7ffc02f933274fcd7bdb0b7f0
marlinarnz <42812798+marlinarnz@users.noreply.github.com>
2019-01-10 14:46:09 +0100

    Finished constructor of staff class

--- app/dataclasses.py
+++ app/dataclasses.py
@@ -1,3 +1,5 @@
+from app.constants import Errors as e
+
 class Staff:
     ''' Object representing a staff member with roles, wishes and other specs'''
 
@@ -8,4 +10,24 @@ class Staff:
         :param jobs: list of jobs that can be attended
         :param wishes: dict with date objects as keys and a priorities as values
         :param prio_job: job where this person is prioritized'''
-        pass
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise ValueError(e.WRONG_STAFF_NAME)
+        if isinstance(workdays, int):
+            self.workdays = workdays
+        else:
+            raise ValueError(e.WRONG_STAFF_WORKDAYS)
+        if isinstance(jobs, list):
+            self.jobs = jobs
+        else:
+            raise ValueError(e.WRONG_STAFF_JOBS)
+        if isinstance(wishes, int):
+            self.wishes = wishes
+        else:
+            raise ValueError(e.WRONG_STAFF_WISHES)
+        if prio_job is not None:
+            if isinstance(prio_job, str) and prio_job in self.wishes:
+                self.prio_job = prio_job
+            else:
+                raise ValueError(e.WRONG_STAFF_PRIO_JOB)
